c67c8d19a0621e5b0cb991cd05e22412a5787e14
gabriel_colque <gabriel.colque.98@gmail.com>
2026-07-14 21:30:47 -0300

    Eliminar componente obsoleto `Notice` y su exportación para limpiar la base de código.

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,4 @@
 export { FilterSelect } from "@/components/common/filter-select";
 export { ListPagination, usePagination } from "@/components/common/list-pagination";
-export { Notice } from "@/components/common/notice";
 export { PageBreadcrumbs } from "@/components/common/page-breadcrumbs";
 export { PageHeader } from "@/components/common/page-header";
